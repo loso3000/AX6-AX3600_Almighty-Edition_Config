3f40333e351edd567c7a14506421c18c432b54ee
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-21 23:51:07 +0800

    16

--- script/diy2im.sh
+++ script/diy2im.sh
@@ -16,9 +16,9 @@ rm -rf feeds/*/*/{luci-app-adguardhome,luci-app-appfilter,open-app-filter,luci-a
 git clone https://github.com/sirpdboy/build.git ./package/build
 git clone https://github.com/sirpdboy/sirpdboy-package ./package/diy
 git clone https://github.com/loso3000/other ./package/other
-
-sed -i 's/7872k/16064k/g' ./target/linux/ramips/image/mt76x8.mk
-sed -i 's/7b0000/fb0000/g' ./target/linux/ramips/dts/mt7628an_alfa-network_awusfree1.dts
+#改镜像大小8M改16M
+# sed -i 's/7872k/16064k/g' ./target/linux/ramips/image/mt76x8.mk
+# sed -i 's/7b0000/fb0000/g' ./target/linux/ramips/dts/mt7628an_alfa-network_awusfree1.dts
 
 # rm -rf  ./package/build/luci-app-netspeedtest
 rm -rf  package/emortal/autocore  && svn co https://github.com/sirpdboy/build/trunk/autocore ./package/lean/autocore

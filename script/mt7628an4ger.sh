5270a4ba44184ea840da8d713c1a7d5ead286802
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-23 10:26:22 +0800

    xx

--- script/mt7628an4ger.sh
+++ script/mt7628an4ger.sh
@@ -4,8 +4,8 @@
 git clone https://github.com/loso3000/other ./package/other
 
 #add7628 er1
-# rm -rf  ./target/linux/ramips
-cp -rf ./package/other/patch/mt7628/ramips/*  ./target/linux/ramips/*
+ rm -rf  ./target/linux/ramips
+cp -rf ./package/other/patch/mt7628/ramips  ./target/linux/ramips
 # 改为16兆
 sed -i 's/7872k/16064k/g' ./target/linux/ramips/image/mt76x8.mk
 sed -i 's/7b0000/fb0000/g' ./target/linux/ramips/dts/mt7628an_alfa-network_awusfree1.dts

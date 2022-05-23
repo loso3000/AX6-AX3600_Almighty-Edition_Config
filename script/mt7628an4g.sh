6e5262d6972c194184247d29874aaf767bb55180
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-23 21:39:03 +0800

    xx

--- script/mt7628an4g.sh
+++ script/mt7628an4g.sh
@@ -4,8 +4,10 @@
 rm -rf feeds/*/*/{luci-app-appfilter,open-app-filter,luci-app-openclash,wrtbwmon,adguardhome,luci-app-timecontrol,luci-app-smartinfo,luci-app-syncdial}
 rm -rf feeds/*/*/{luci-app-ssr-plus,netdata,smartdns,luci-app-adguardhome,luci-app-smartdns,luci-app-beardropper,luci-app-wrtbwmon}
 
-svn export https://github.com/loso3000/other/trunk/patch/mt7628/e-ruike/quectel-CM/ package/new/quectel-CM
-
+svn export https://github.com/loso3000/other/trunk/patch/quectel-CM package/new/quectel-CM
+svn export https://github.com/loso3000/other/trunk/patch/er7628 package/er7628
+cp -rf package/er7628/mt76x8.mk      ./target/linux/ramips/image/mt76x8.mk
+cp -rf package/er7628/ER-MF7628Q1   ./target/linux/ramips/dts/ER-MF7628Q1
 #改镜像大小8M改16M awusfree1
 sed -i 's/7872k/16064k/g' ./target/linux/ramips/image/mt76x8.mk
 sed -i 's/7b0000/fb0000/g' ./target/linux/ramips/dts/mt7628an_alfa-network_awusfree1.dts

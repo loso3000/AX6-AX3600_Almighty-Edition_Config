fa100d5f539cc9cbe11ca59c758e2d541fa0b6a7
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-05-24 11:44:16 +0800

    d

--- script/mt7628an4g.sh
+++ script/mt7628an4g.sh
@@ -7,7 +7,7 @@ rm -rf feeds/*/*/{luci-app-ssr-plus,netdata,smartdns,luci-app-adguardhome,luci-a
 svn export https://github.com/loso3000/other/trunk/patch/quectel-CM package/new/quectel-CM
 svn export https://github.com/loso3000/other/trunk/patch/er7628 package/er7628
 cp -rf package/er7628/mt76x8.mk      ./target/linux/ramips/image/mt76x8.mk
-cp -rf package/er7628/ER-MF7628Q1   ./target/linux/ramips/dts/ER-MF7628Q1
+cp -rf package/er7628/mt7628an_er-mf7628q1.dts  ./target/linux/ramips/dts/
 #改镜像大小8M改16M awusfree1
 sed -i 's/7872k/16064k/g' ./target/linux/ramips/image/mt76x8.mk
 sed -i 's/7b0000/fb0000/g' ./target/linux/ramips/dts/mt7628an_alfa-network_awusfree1.dts
